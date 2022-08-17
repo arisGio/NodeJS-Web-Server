d77f0400d297fafc4a4a800f3cc371a010278f80
aris_giotis <apjoti@gmail.com>
2022-08-17 13:17:19 +0300

    Server responding 'Hello Node' on localhost:3000

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const http = require('http')
 const port = 3000
 
 const server = http.createServer(function(req, res) {
-
+    res.write('Hello Node')
+    res.end()
 })
 
 server.listen(port, function(error) {
